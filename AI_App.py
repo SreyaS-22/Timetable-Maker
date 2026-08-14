0311d57c5c6663dec670f3ae4637c59f2fc48f73
SreyaS-22 <sreyasankar2021@outlook.com>
2026-08-14 20:52:33 +0530

    Add main menu and input validation
    Created a menu system for navigating between the AI chatbot,
    revision timetable feature, and exit option.
    Added error handling for invalid user input.

--- AI_App.py
+++ AI_App.py
@@ -2,7 +2,28 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+def AI_Chatbot():
+
+    while True:
+
+        request = input(name + ": How can I help? Please text 'quit' if you're done.\nYou:")
+
+        if request.lower() == 'quit':
+            print("Ok. Thank you for your time. See you soon. Byeeee! :)")
+            break
+
+        else:
+            response = client.responses.create(
+                model = "gpt-5-mini",
+                input = request
+                )
+
+        print(name, ": ", response.output_text)
+
+    
 load_dotenv()
+
+
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -12,21 +33,26 @@ name = input("What would you like my name to be?")
 
 print("Hi! My name is", name)
 
-
+print("Menu: \nOption 1: Chat with me \nOption 2: Create a revision timetable \nOption 3: Quit")
 
 while True:
+    try:
+        user_input = int(input("What would you like to do? Type in the number:"))
 
-    request = input(name + ": How can I help? Please text 'quit' if you're done.\n You:")
+    except ValueError:
+        print("Invalid input entered.")
+        continue
 
-    if request.lower() == 'quit':
-        print("Ok. Thank you for your time. See you soon. Byeeee! :)")
+    if user_input == 1:
+        AI_Chatbot()
+        break
+    elif user_input == 2:
+        print("Revision timetable feature coming soon")
+        break
+
+    elif user_input == 3:
+        print("Thank you for your time. See you soon. Byeeee! :)")
         break
 
     else:
-        response = client.responses.create(
-            model = "gpt-5-mini",
-            input = request
-            )
-
-    print(name, ": ", response.output_text)
-  
+        print("Invalid input entered.")
